c93742d5b784b3ff1cc7c43c7ce3dd31922df006
Cruz <cruz.delacruz37@gmail.com>
2019-05-06 15:59:15 -0400

    I am using the checkWinTest file to test out a function.

--- ticTacToe.py
+++ ticTacToe.py
@@ -15,6 +15,23 @@ def printBoard(board):
     print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
 
+def checkWinner(board):
+    ttVals = []
+    ttFinal = []
+    inner = []
+
+    for v in board.values():
+        ttVals.append(v)
+
+    for i in range(len(ttVals)):
+        inner.append(ttVals[i])
+        if (i+1) % 3 == 0:
+            ttFinal.append(inner)
+            inner = []
+
+    print(ttFinal)
+
+
 turn = 'X'
 for i in range(9):
     printBoard(theBoard)
@@ -27,3 +44,4 @@ for i in range(9):
         turn = 'X'
 
 printBoard(theBoard)
+checkWinner(theBoard)
