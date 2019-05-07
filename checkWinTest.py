e22e37bbf7a2bed2ec2a026e992429036ec5cad6
Cruz <cruz.delacruz37@gmail.com>
2019-05-07 01:42:02 -0400

    we can check for vertical and horizontal wins now, very rough and gross though

--- checkWinTest.py
+++ checkWinTest.py
@@ -1,6 +1,6 @@
-theBoard = {'top-L': 'O', 'top-M': 'X', 'top-R': 'O',
-            'mid-L': 'X', 'mid-M': 'O', 'mid-R': 'X',
-            'low-L': 'O', 'low-M': 'X', 'low-R': 'O'}
+theBoard = {'top-L': 'O', 'top-M': 'O', 'top-R': 'O',
+            'mid-L': 'X', 'mid-M': 'O', 'mid-R': 'O',
+            'low-L': 'O', 'low-M': 'X', 'low-R': 'X'}
 
 
 def ValsList(board):
@@ -13,7 +13,50 @@ def ValsList(board):
             tt.append(inner)
             inner = []
 
-    print(tt)
+    return(tt)
+
+def win_hor(list, turn):
+    x, y = 0, 0
+    count = 0
+    won = False
+
+    # this for loop checks for a horizontal win. it does check the third row, but it doesn't
+    # report for the last line since the loop ends right after it checks the last line
+    # and my condition for the win is within the for loop.
+    for x in range(len(list)):
+        if count == 3:
+            won = True
+            count = 0 # if there is a win on lines 1 or 2, count would still be set to 3 after the break and i would get the winning message printed twice
+            break
+        else:
+            count = 0
+        for y in range(len(list[x])):
+            if list[x][y] == turn:
+                count += 1
+    # this if statement here checks the count to see if the last line is a win
+    if count == 3:
+        won = True
+
+def win_ver(list, turn):
+    x, y = 0, 0
+    count = 0
+    won = False
+
+    for y in range(len(list[x])):
+        if count == 3:
+            won = True
+            count = 0  # if there is a win on lines 1 or 2, count would still be set to 3 after the break and i would get the winning message printed twice
+            break
+        else:
+            count = 0
+        for x in range(len(list)):
+            if list[x][y] == turn:
+                count += 1
+        if count == 3:
+            won = True
 
 
-ValsList(theBoard)
+
+print(ValsList(theBoard))
+win_hor(ValsList(theBoard), 'O')
+win_ver(ValsList(theBoard), 'O')
--- ticTacToe.py
+++ ticTacToe.py
@@ -25,8 +25,45 @@ def ValsList(board):
             tt.append(inner)
             inner = []
 
-    print(tt)
+    return(tt)
 
+def win_hor(list, turn):
+    x, y = 0, 0
+    count = 0
+
+    # this for loop checks for a horizontal win. it does check the third row, but it doesn't
+    # report for the last line since the loop ends right after it checks the last line
+    # and my condition for the win is within the for loop.
+    for x in range(len(list)):
+        if count == 3:
+            print(turn + ' has won horizontally!')
+            count = 0 # if there is a win on lines 1 or 2, count would still be set to 3 after the break and i would get the winning message printed twice
+            break
+        else:
+            count = 0
+        for y in range(len(list[x])):
+            if list[x][y] == turn:
+                count += 1
+    # this if statement here checks the count to see if the last line is a win
+    if count == 3:
+        print(turn + ' has won horizontally!')
+
+def win_ver(list, turn):
+    x, y = 0, 0
+    count = 0
+
+    for y in range(len(list[x])):
+        if count == 3:
+            print(turn + ' has won vertically!')
+            count = 0  # if there is a win on lines 1 or 2, count would still be set to 3 after the break and i would get the winning message printed twice
+            break
+        else:
+            count = 0
+        for x in range(len(list)):
+            if list[x][y] == turn:
+                count += 1
+        if count == 3:
+            print(turn + ' has won vertically!')
 
 turn = 'X'
 for i in range(9):
@@ -38,6 +75,12 @@ for i in range(9):
         turn = 'O'
     else:
         turn = 'X'
+    if win_hor(ValsList(theBoard), turn):
+        print(turn + ' has onw horizontally!')
+        break
+    if win_ver(ValsList(theBoard), turn):
+        print(turn + ' has one vertically!')
+        break
 
 printBoard(theBoard)
 print()
